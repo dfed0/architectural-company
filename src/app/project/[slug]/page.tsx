0e00eaec09c6675e9074a67b1e5b4ee44a53dfb7
dfed0 <denisfedorchuk2004@gmail.com>
2025-06-10 20:56:27 +0300

    fix 3

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import { Suspense } from 'react'
 import { WindowSizeProvider } from '@/app/contexts/WindowSizeContext'
 // import GalleryPage from '@/app/gallerypage/GalleryPage'
-import GalleryPage from '@/app/gallerypage/GalleryPage'
+import GalleryPage from '../../gallerypage/GalleryPage'
 // import * as cheerio from 'cheerio'
 export default function GalleryOfProject() {
   return (
