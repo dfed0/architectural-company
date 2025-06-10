2c98f112ceb05fdc482ed1885652227bbe534cc1
dfed0 <denisfedorchuk2004@gmail.com>
2025-06-10 23:22:34 +0300

    fix fix

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Suspense } from 'react'
-import { WindowSizeProvider } from '@/app/contexts/WindowSizeContext'
+import { WindowSizeProvider } from '../../../app/contexts/WindowSizeContext'
 // import GalleryPage from '@/app/gallerypage/GalleryPage'
 import GalleryPage from '../../gallerypage/GalleryPage'
 // import * as cheerio from 'cheerio'
