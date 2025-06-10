7ea86db42188b4838f6af27a9b0e197e3fcc8535
dfed0 <denisfedorchuk2004@gmail.com>
2025-06-10 20:52:29 +0300

    pages 2

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Suspense } from 'react'
 import { WindowSizeProvider } from '@/app/contexts/WindowSizeContext'
+// import GalleryPage from '@/app/gallerypage/GalleryPage'
 import GalleryPage from '@/app/gallerypage/GalleryPage'
 // import * as cheerio from 'cheerio'
 export default function GalleryOfProject() {
