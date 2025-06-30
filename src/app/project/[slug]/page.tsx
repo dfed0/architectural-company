0207839c1aa9d17bb100982dd960ce69ff3a42bb
dfed0 <denisfedorchuk2004@gmail.com>
2025-06-30 16:44:28 +0300

    1

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { Suspense } from 'react'
 import { WindowSizeProvider } from '../../../app/contexts/WindowSizeContext'
-import GalleryPage from '../../gallerypage/GalleryPage'
+import GalleryPage from '../../Gallerypage/GalleryPage'
 import { I18nextProvider } from 'react-i18next'
 import i18next from '../../i18n/translations/i18next'
 import { GoogleDataProvider } from '@/app/contexts/GoogleDataContext'
