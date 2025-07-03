3ef1d61b11d3bccc471fae860b47b0fbf79f2e48
dfed0 <denisfedorchuk2004@gmail.com>
2025-07-03 19:15:41 +0300

    Final

--- src/app/components/ServiceCard.tsx
+++ src/app/components/ServiceCard.tsx
@@ -23,14 +23,12 @@ const ServiceCard = ({
 }: ServiceCardProps) => {
   const { t } = useTranslation()
   return (
-    <div className="flex flex-col items-start self-start gap-[2.5rem] sm:w-full flex-[1_0_0]">
+    <div className="z-10 flex flex-col items-start self-start gap-[2.5rem] sm:w-full flex-[1_0_0]">
       <Image
         src={imageUrl}
         alt={altText}
         height={420}
         width={405}
-        // objectFit='cover'
-        // width={auto}
         style={{ width: '100%', height: '26.25rem', objectFit: 'cover' }}
         className="rounded-[0.75rem] border-2"
       />
@@ -38,7 +36,6 @@ const ServiceCard = ({
         <p className="text-[#000] overflow-ellipsis text-[1.25rem] font-[Inter_Var] font-[400] leading-[1.5rem]">
           {title}
         </p>
-        {/* <p className="text-[#000] font-[Inter_Var] text-[0.9375rem] font-[400] leading-[1.25rem] self-stretch"> */}
         <p className="text-[#00000099] overflow-ellipsis text-[1.0625rem] font-[Inter_Var] font-[400]">
           {subtitle}
         </p>
